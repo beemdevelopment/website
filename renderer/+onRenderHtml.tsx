153080758287c9a598bed09ad63f189629a326ee
Michael Schättgen <michael@schattgen.me>
2024-04-28 16:45:50 +0200

    Fix logo url and meta tags

--- renderer/+onRenderHtml.tsx
+++ renderer/+onRenderHtml.tsx
@@ -4,7 +4,7 @@ export { onRenderHtml };
 import ReactDOMServer from "react-dom/server";
 import { PageShell } from "./PageShell";
 import { escapeInject, dangerouslySkipEscape } from "vike/server";
-import logoUrl from "./logo.svg";
+import Icon from "../assets/icon.png";
 import type { OnRenderHtmlAsync } from "vike/types";
 
 const onRenderHtml: OnRenderHtmlAsync = async (pageContext): ReturnType<OnRenderHtmlAsync> => {
@@ -20,17 +20,19 @@ const onRenderHtml: OnRenderHtmlAsync = async (pageContext): ReturnType<OnRender
 
   // See https://vike.dev/head
   const { documentProps } = pageContext.exports;
-  const title = (documentProps && documentProps.title) || "Vite SSR app";
-  const desc = (documentProps && documentProps.description) || "App using Vite + Vike";
 
   const documentHtml = escapeInject`<!DOCTYPE html>
     <html lang="en">
     <head>
       <meta charset="UTF-8" />
-      <link rel="icon" type="image/svg+xml" href="/assets/icon.png" />
+      <link rel="icon" type="image/svg+xml" href="${Icon}" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
       <title>Aegis Authenticator</title>
+      <meta name="description" content="Aegis Authenticator is a free, secure and open source app for Android to manage your 2-step verification tokens for your online services."/>
+      <meta name="application-name" content="Aegis Authenticator" />
+      <meta name="author" content="Beem Development" />
+      <link rel="author" href="https://github.com/beemdevelopment" />
     </head>
     <body>
       <div id="react-root">${dangerouslySkipEscape(pageHtml)}</div>
